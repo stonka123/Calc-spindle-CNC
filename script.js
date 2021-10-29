77d752afc1c7cb7b328462e864c0c502f304baba
stonka123 <stonk123@github.com>
2021-10-29 20:58:21 +0200

    added dark mode and repair some problems

--- script.js
+++ script.js
@@ -3,26 +3,24 @@ const material = document.querySelector('#material');
 const spindleScore = document.querySelector('.spindle-rate');
 const calcBtn = document.querySelector('button');
 const errorShow = document.querySelector('.error-info');
+const body = document.querySelector('body');
+const toggleBtn = document.getElementById('toggleMode');
+const spindleInfo = document.querySelector('.spindle-info');
 
 const calculate = () => {
 	errorShow.style.visibility = 'hidden';
+	spindleInfo.style.visibility = 'visible';
 	spindleScore.textContent = '';
 	const patter = (3.14 * diameter) / 25000;
 	const score = parseFloat(material.value);
 	const diam = parseFloat(diameter.value);
-	// console.log(diameter.value);
-	// console.log(material.value);
 	let box = 3.14 * diameter.value;
-	// console.log(box);
-	let wynik = (25000 / box) * material.value;
-	// console.log(wynik);
-
-	spindleScore.textContent = `${wynik.toFixed(0)}`;
-	checkInf();
+	let totalyScore = (25000 / box) * material.value;
+	spindleScore.textContent = `${totalyScore.toFixed(0)}`;
 	checkString();
+	checkInf();
 };
 
-// errorShow.style.visibility = 'visible';
 const checkInf = () => {
 	if (diameter.value === '') {
 		spindleScore.textContent = '';
@@ -30,6 +28,8 @@ const checkInf = () => {
 		errorShow.style.visibility = 'visible';
 		errorShow.textContent = 'Wpisz odpowiednią średnicę wiertła!';
 		spindleScore.textContent = '';
+
+		spindleInfo.style.visibility = 'hidden';
 	} else if (diameter.value == 0) {
 		errorShow.style.visibility = 'visible';
 		spindleScore.textContent = '';
@@ -39,12 +39,20 @@ const checkInf = () => {
 const checkString = () => {
 	const re = new RegExp('[^abc]');
 	if (diameter.value.match(re)) {
-		console.log('ok');
 	} else {
-		console.log('blad');
 		errorShow.style.visibility = 'visible';
 		errorShow.textContent = 'Błąd średnicy narzędzia!';
+		spindleInfo.style.visibility = 'hidden';
 	}
 };
 
+const handleDarkMode = () => {
+	if (body.getAttribute('data-mode') === 'light') {
+		body.setAttribute('data-mode', 'dark');
+	} else {
+		body.setAttribute('data-mode', 'light');
+	}
+};
+
+toggleBtn.addEventListener('click', handleDarkMode);
 calcBtn.addEventListener('click', calculate);
